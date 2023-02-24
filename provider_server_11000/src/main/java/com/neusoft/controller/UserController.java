5bdf07112f847cfeed4789570dc87dc12f3c2ebb
Cogit0ErgoSum <songyang20202020@163.com>
2023-02-24 19:52:14 +0800

    Bus finished

--- provider_server_11000/src/main/java/com/neusoft/controller/UserController.java
+++ provider_server_11000/src/main/java/com/neusoft/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.neusoft.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +12,14 @@ import com.neusoft.po.User;
 
 @RestController
 @RequestMapping("/user")
+@RefreshScope		//开启动态刷新
 public class UserController {
+	
+	@Value("${msg}")
+	private String msg;
 	
 	@GetMapping("/getUserById/{userId}")
 	public CommonResult<User> getUserById(@PathVariable("userId") Integer userId) {
-		return new CommonResult(200, "success(11000)", new User(userId, "张三", "123"));
+		return new CommonResult(200, "success(11000) (msg:"+msg+")", new User(userId, "张三", "123"));
 	}
 }
